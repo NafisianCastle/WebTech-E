227e553d6321ccbc4895fbba80473f7f5bb7342b
NafisianCastle <nafis.cseaiub@gmail.com>
2020-12-17 04:07:36 +0600

    commit

---
+++ Lab Task-8/admin/adminChangePassword.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+if ($_SESSION['username'] == "" || $_SESSION["usertype"] != "admin") {
+    header("location:adminLogin.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/adminDashboard.css">
+    <link rel="stylesheet" type="text/css" href="css/header_admin.css">
+    <link rel="stylesheet" type="text/css" href="css/sidebar_admin.css">
+    <link rel="stylesheet" type="text/css" href="../HnF/Footer.css">
+</head>
+
+<body>
+    <?php
+    include 'header_admin.html';
+    include 'sidebar_admin.html';
+    ?>
+    <div class="main">
+        <form method="post" action="#">
+            <div class="form-input">
+                <label>Old Password</label>
+                <br>
+                <input type="text" name="password" value="2524" disabled>
+                <label class="text_error"><?php echo $passwordErr; ?></label>
+            </div>
+            <div class="form-input">
+                <label>Enter Password</label>
+                <br>
+                <input type="password" name="password" placeholder="Password">
+                <label class="text_error"><?php echo $passwordErr; ?></label>
+            </div>
+            <div class="form-input">
+                <label>Re-type Password</label>
+                <br>
+                <input type="password" name="retypePassword" placeholder="Password">
+                <label class="text_error"><?php echo $passwordErr; ?></label>
+            </div>
+
+            <div>
+                <input type="submit" value="Save Change" name="submit">
+
+            </div>
+        </form>
+    </div>
+
+
+    <?php
+    include '../HnF/Footer.php';
+    ?>
+
+</body>
+
+
+</html>
---
+++ Lab Task-8/admin/usersData.php
@@ -0,0 +1,218 @@
+<?php
+session_start();
+if ($_SESSION['username'] == "" || $_SESSION["usertype"] != "admin") {
+     header("location:adminLogin.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" type="text/css" href="css/adminDashboard.css">
+     <link rel="stylesheet" type="text/css" href="css/header_admin.css">
+     <link rel="stylesheet" type="text/css" href="css/sidebar_admin.css">
+     <link rel="stylesheet" type="text/css" href="../HnF/Footer.css">
+     <style>
+          #list {
+               font-family: Arial, Helvetica, sans-serif;
+               border-collapse: collapse;
+               width: 100%;
+          }
+
+          #list td,
+          #list th {
+               border: 1px solid #ddd;
+               padding: 8px;
+          }
+
+          #list tr:nth-child(even) {
+               background-color: #f2f2f2;
+          }
+
+          #list tr:hover {
+               background-color: #ddd;
+          }
+
+          #list th {
+               padding-top: 12px;
+               padding-bottom: 12px;
+               text-align: left;
+               background-color: dodgerblue;
+               color: white;
+          }
+     </style>
+</head>
+
+<body>
+     <?php
+     include 'header_admin.html';
+     include 'sidebar_admin.html';
+     ?>
+
+     <div class="main">
+          <form>
+               <br>
+               <table id="list">
+                    <tr>
+                         <th>ID</th>
+                         <th>Username</th>
+                         <th>Name</th>
+                         <th>Password</th>
+                         <th>Email</th>
+                         <th>Phone</th>
+                         <th>Address</th>
+                         <th>City</th>
+                         <th>Counry</th>
+
+                         <th>DOB</th>
+                         <th>Image</th>
+                    </tr>
+
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php">Edit</a></td>
+                         <td><a href="deleteUser.php">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                         <td>1</td>
+                         <td>nafis</td>
+                         <td>nafisur Rahman</td>
+                         <td>1234</td>
+                         <td>nfa@gmail.com</td>
+                         <td>012345678902</td>
+                         <td>khilgaon</td>
+                         <td>Dhaka</td>
+                         <td>Bangladesh</td>
+                         <td>12-03-99</td>
+                         <td>null</td>
+
+                         <td><a href="editUser.php?name=<?php echo $User['Name'] ?>">Edit</a></td>
+                         <td><a href="deleteUser.php?name=<?php echo $User['Name'] ?>">Delete</a></td>
+
+                    </tr>
+
+               </table>
+          </form>
+     </div>
+     <?php
+     include '../HnF/Footer.php';
+     ?>
+</body>
+
+</html>
